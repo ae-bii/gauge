d3a9fc558ae8c00c9e68744a8e2c4648a4486bd4
ae-bii <ab5494@columbia.edu>
2025-10-13 13:36:20 -0500

    created a simple frontend using ppxlib

---
+++ lib/frontend.ml
@@ -0,0 +1,20 @@
+open Ppxlib
+
+let parse_structure ~filename source =
+  let lexbuf = Lexing.from_string source in
+  lexbuf.Lexing.lex_curr_p <- { lexbuf.Lexing.lex_curr_p with Lexing.pos_fname = filename };
+  Parse.implementation lexbuf
+
+let contains_rec structure =
+  let open Parsetree in
+  List.exists (fun item ->
+    match item.pstr_desc with
+    | Pstr_value (Asttypes.Recursive, _bindings) -> true
+    | _ -> false
+  ) structure
+
+let infer_from_source ~filename source =
+  try
+    let str = parse_structure ~filename source in
+    if contains_rec str then Cost_model.On else Cost_model.O1
+  with _ -> Cost_model.OUnknown
