a832c9bb692b68c3c49f79bc6dbd95f9cb319bce
ae-bii <ab5494@columbia.edu>
2025-10-18 10:59:14 -0500

    added tests for higher order functions

--- test/test_higher_order.ml
+++ test/test_higher_order.ml
@@ -2,4 +2,4 @@ let%expect_test "helper passed to List.map" =
   let code = "let f n = let rec go k = if k <= 0 then 0 else go (k-1) in List.map go (List.init n (fun i -> i))" in
   let inferred = Gauge.Infer.infer_of_string_code code in
   Printf.printf "%s\n" (Gauge.Cost_model.to_string inferred);
-  [%expect {| O(n) |}]
+  [%expect {| O(n^2) |}]
